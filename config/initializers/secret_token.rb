5b9a1b5531f6bac147865d7fe9f6e11f0e518779
Nathan Ruska <nruska@gmail.com>
2011-10-17 20:04:48 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mediahaste::Application.config.secret_token = '5c5f092d371052d079032582a0d9aa9b9e4c66f8de9775316da89c48515c530ef806cd2b0bb7d059eedfde9d8cefdaf35a45d8a34a684865d4c0f36b0d2e696c'

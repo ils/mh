e8a4412ec2f94cb74c527ce7e2d2c7257596c7ac
unknown <tfadmin@tfbox20.(none)>
2011-11-10 20:48:17 -0700

    added basic article specs

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ActiveRecord::Base
+  attr_accessible :headline, :body, :date
+  
+  validates :headline, :presence => true,
+                       :length => { :maximum => 150 }
+  validates :body, :presence => true,
+                   :length => { :maximum => 5000 }
+                   
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Article do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  headline = "Sample Heading Appears on Local Website"
+  body = "body"
+  date = "2011-11-10 08:05:01"
+  
+  before(:each) do
+    @article = { :headline => "headline", :body => "body" }
+  end
+  
+  it "should create a new instance given valid attributes" do
+    Article.create!(@article)
+  end
+  
+  it "should require a headline" do
+    headless_article = Article.new(@article.merge(:headline => ""))
+    headless_article.should_not be_valid
+  end
+  
+  it "should require text in the body" do
+    no_body_article = Article.new(@article.merge(:body => ""))
+    no_body_article.should_not be_valid
+  end
+  
 end
